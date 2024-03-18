c734ef0e774a537bb84cc98fec94d07945e03686
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-03-17 20:24:28 -0400

    updated

--- testes/tests/Browser/RegisterUserTest.php
+++ testes/tests/Browser/RegisterUserTest.php
@@ -18,5 +18,39 @@ class RegisterUserTest extends DuskTestCase
         });
     }
 
+    /** @test*/
+    public function check_if_login_function_is_working()
+    {
+        $userTest = new User();
+        $userTest->name = 'Usuario de teste';
+        $userTest->email = 'teste@gmail.com';
+        $userTest->password = bcrypt('123');
+        $userTest->save();
 
+        $this->browse(function (Browser $browser) {
+            $browser->visit('login')
+                ->type('email', 'teste@gmail.com')
+                ->type('password', '123')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+
+        $userTest->delete();
+    }
+
+    /** @test */
+    public function check_if_register_function_is_working()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->visit('/register')
+                ->type('name', 'Usuario de teste')
+                ->type('email', 'test@gmail.com')
+                ->type('password', '123')
+                ->press('Registrar')
+                ->assertPathIs('/login')
+                ->assertSee('Usuario criado com sucesso');
+        });
+        $userTest = User::where('email', 'test@gmail.com')->first();
+        $userTest->delete();
+    }
 }
